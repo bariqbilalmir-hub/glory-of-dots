c5025602577c3c7324497f0a73dcdd0473a93dfa
bariqbilalmir-hub <bariqbilalmir@gmail.com>
2026-07-19 16:16:59 +0530

    feat: connect map rendering and node interaction

--- js/game.js
+++ js/game.js
@@ -1,7 +1,22 @@
 import { Renderer } from './renderer.js';
+import { createMap } from './map.js';
+import { Input } from './input.js';
 
 export class Game {
- constructor(canvas){ this.canvas=canvas; this.renderer=new Renderer(canvas); this.running=false; }
- start(){ this.running=true; this.loop(); }
- loop(){ if(!this.running) return; this.renderer.render(); requestAnimationFrame(()=>this.loop()); }
+ constructor(canvas){
+  this.canvas=canvas;
+  this.renderer=new Renderer(canvas);
+  this.nodes=createMap();
+  this.input=new Input(canvas);
+  this.selected=null;
+  canvas.addEventListener('click',()=>this.selectNode());
+ }
+ selectNode(){
+  this.selected=this.nodes.find(n=>n.contains(this.input.mouse.x,this.input.mouse.y)) || null;
+ }
+ start(){this.loop();}
+ loop(){
+  this.renderer.render(this.nodes,this.selected);
+  requestAnimationFrame(()=>this.loop());
+ }
 }
